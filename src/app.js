0c161d9695cefe71567d58177c5c1d5cae258e85
AneeshBist <aneeshbist@gmail.com>
2023-09-25 23:59:05 +0530

    inital file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+const { log } = require("console");
+const express = require("express");
+const app = express();
+const port = 8000;
 
+app.get("", (req, res) => {
+  res.send("welcome to The Website");
+});
+
+app.listen(port, () => {
+  console.log(`listening to the port ${port}`);
+});
