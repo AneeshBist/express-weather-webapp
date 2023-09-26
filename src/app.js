34b7d29a05fde661135a1542e7c1d93ab63952f7
AneeshBist <aneeshbist@gmail.com>
2023-09-26 23:51:41 +0530

    routing added

--- src/app.js
+++ src/app.js
@@ -4,7 +4,19 @@ const app = express();
 const port = 8000;
 
 app.get("", (req, res) => {
-  res.send("welcome to The Website");
+  res.send("welcome to The Weather Website");
+});
+
+app.get("/about", (req, res) => {
+  res.send("welcome to About section of the Website");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("welcome to Weather section of the Website");
+});
+
+app.get("*", (req, res) => {
+  res.send("Oopps !!! 404 error page");
 });
 
 app.listen(port, () => {
