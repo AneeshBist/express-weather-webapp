b4667fac67ec007e5868512b3c3ce3d9432f692a
AneeshBist <aneeshbist@gmail.com>
2023-09-30 22:59:43 +0530

    using partials

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 const { log } = require("console");
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
 const app = express();
 const port = 8000;
 
 //public static path
 const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
 app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
+
 app.use(express.static(static_path));
 
 //routing
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index");
   //res.send("welcome to The Weather Website");
 });
