55eaf51b8b9bed48b7774c3117733a822945758c
AneeshBist <aneeshbist@gmail.com>
2023-10-03 23:40:08 +0530

    fetching weather details from api

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,26 @@ const submitBtn = document.getElementById("submit-btn");
 const cityName = document.getElementById("city-name");
 
 const cityOutput = document.getElementById("city-output");
+const temp = document.getElementById("temp");
+const tempStats = document.getElementById("temp-stats");
 
 const getInfo = async (event) => {
   event.preventDefault();
   let cityVal = cityName.value;
   if (cityVal === "") {
-    cityOutput.innerText = `Please write city name`;
+    cityOutput.innerText = `Please write a city name`;
   } else {
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=Dehradun,in&units=metric&appid=219e680aeb5a2cab7cf3cccec33e903e`;
-    const res = await fetch(url);
+    try {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=219e680aeb5a2cab7cf3cccec33e903e`;
+      const res = await fetch(url);
+      const data = await res.json();
+      const arrData = [data];
+      temp.innerText = arrData[0].main.temp;
+      tempStats.innerText = arrData[0].weather[0].main;
+      console.log(arrData[0].weather[0].main);
+    } catch {
+      cityOutput.innerText = `Please check the city name`;
+    }
   }
 };
 
